f001517b906604a3f4e84e74a5c211efb938e977
Katie Ediger <katieediger@gmail.com>
2018-05-25 15:47:25 -0500

    Added solution for FizzBuzz problem

--- chapter1.js
+++ chapter1.js
@@ -3,3 +3,17 @@ while (hash.length <= 7) {
   console.log(hash);
   hash = hash + '#';
 }
+
+let number = 1;
+while (number <= 100) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (number % 3 == 0) {
+    console.log('Fizz');
+  } else if (number % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(number);
+  }
+  number = number + 1;
+}
