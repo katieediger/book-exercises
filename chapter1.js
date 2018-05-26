e3e5db2b042a4f72aa60cc14f17864820fdc2a5e
Katie Ediger <katieediger@gmail.com>
2018-05-25 22:36:58 -0500

    Added solution for problem 3

--- chapter1.js
+++ chapter1.js
@@ -17,3 +17,18 @@ while (number <= 100) {
   }
   number = number + 1;
 }
+
+let board = "";
+let size = 8
+
+for (let y = 0; y < size; y++) { //This is the outer loop that creates new rows
+  for (let x = 0; x < size; x++) { //This is the inner loop that determines what is displayed in each line
+    if ((x+y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
