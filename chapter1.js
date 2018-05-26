a744f34813345ae1b446bb62e008f212a38e9c64
Katie Ediger <katieediger@gmail.com>
2018-05-26 06:23:30 -0500

    Switched out single quotes for double quotes

--- chapter1.js
+++ chapter1.js
@@ -1,17 +1,17 @@
-let hash = '#';
+let hash = "#";
 while (hash.length <= 7) {
   console.log(hash);
-  hash = hash + '#';
+  hash = hash + "#";
 }
 
 let number = 1;
 while (number <= 100) {
   if (number % 3 == 0 && number % 5 == 0) {
-    console.log('FizzBuzz');
+    console.log("FizzBuzz");
   } else if (number % 3 == 0) {
-    console.log('Fizz');
+    console.log("Fizz");
   } else if (number % 5 == 0) {
-    console.log('Buzz');
+    console.log("Buzz");
   } else {
     console.log(number);
   }
